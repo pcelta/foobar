a090a7fca5b01a3040ad0f27a16820f17ad845c8
Pedro Ribeiro <pcelta.ribeiro@gmail.com>
2014-08-28 00:44:49 -0300

    replaced tab for space

--- src/main/java/com/pcelta/foobar/entity/Transaction.java
+++ src/main/java/com/pcelta/foobar/entity/Transaction.java
@@ -15,52 +15,56 @@ import javax.persistence.Table;
 @Table(name="transactions")
 public class Transaction implements Serializable {
 
-	private static final long serialVersionUID = 8297144483906298344L;
+    private static final long serialVersionUID = 8297144483906298344L;
 
-	@Id
-	@GeneratedValue
-	@Column(name="id")
-	private Integer id;
-	
-	@Column(name="amount")
-	private Double amount;
-	
-	@Column(name="created_at")
-	private Calendar createdAt;
-	
-	@ManyToOne
-	@JoinColumn(name="credit_card_id", referencedColumnName="id")
-	private CreditCard creditCard;
+    @Id
+    @GeneratedValue
+    @Column(name="id")
+    private Integer id;
 
-	public Integer getId() {
-		return this.id;
-	}
+    @Column(name="amount")
+    private Double amount;
 
-	public void setId(Integer id) {
-		this.id = id;
-	}
+    @Column(name="created_at")
+    private Calendar createdAt;
 
-	public Double getAmount() {
-		return this.amount;
-	}
+    @ManyToOne
+    @JoinColumn(name="credit_card_id", referencedColumnName="id")
+    private CreditCard creditCard;
 
-	public void setAmount(Double amount) {
-		this.amount = amount;
-	}
+    public Transaction() {
+        this.createdAt = Calendar.getInstance();
+    }
 
-	public Calendar getCreatedAt() {
-		return this.createdAt;
-	}
+    public Integer getId() {
+        return this.id;
+    }
 
-	public void setCreatedAt(Calendar createdAt) {
-		this.createdAt = createdAt;
-	}
+    public void setId(Integer id) {
+        this.id = id;
+    }
 
-	public CreditCard getCreditCard() {
-		return this.creditCard;
-	}
+    public Double getAmount() {
+        return this.amount;
+    }
 
-	public void setCreditCard(CreditCard creditCard) {
-		this.creditCard = creditCard;
-	}
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
+
+    public Calendar getCreatedAt() {
+        return this.createdAt;
+    }
+
+    public void setCreatedAt(Calendar createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public CreditCard getCreditCard() {
+        return this.creditCard;
+    }
+
+    public void setCreditCard(CreditCard creditCard) {
+        this.creditCard = creditCard;
+    }
 }
