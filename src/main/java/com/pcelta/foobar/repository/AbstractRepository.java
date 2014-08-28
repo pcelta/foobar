864eac373f1a069d0f120bdba5e1c6ab14b57e9b
Pedro Ribeiro <pcelta.ribeiro@gmail.com>
2014-08-28 00:34:18 -0300

    code style

--- src/main/java/com/pcelta/foobar/repository/AbstractRepository.java
+++ src/main/java/com/pcelta/foobar/repository/AbstractRepository.java
@@ -6,13 +6,13 @@ import com.pcelta.foobar.factory.EntityManagerFactory;
 
 public class AbstractRepository {
 
-	private EntityManager entityManager;
-	
-	protected EntityManager getEntityManager() {
-		if (this.entityManager == null) {
-			this.entityManager = EntityManagerFactory.createEntityManager();
-		}
-		
-		return this.entityManager;
-	}
+    private EntityManager entityManager;
+
+    protected EntityManager getEntityManager() {
+        if (this.entityManager == null) {
+            this.entityManager = EntityManagerFactory.createEntityManager();
+        }
+
+        return this.entityManager;
+    }
 }
